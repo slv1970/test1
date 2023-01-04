372a4d4f793c225e81bfaf6d57726c12ab24247c
slv1970 <34186079+slv1970@users.noreply.github.com>
2023-01-04 13:57:48 +0200

    aaa

--- routines/elbaza/__t20455_iu_bef.sql
+++ routines/elbaza/__t20455_iu_bef.sql
@@ -3,7 +3,7 @@ create or replace function elbaza.__t20455_iu_bef() returns trigger
 as
 $$
 begin
-
+-----------------------___________________________________
 	NEW.cache_data = md5(
 		concat( 
 			NEW.idkart, NEW.dttmcr, NEW.dttmup, NEW.dttmcl, NEW.userid, NEW.parent, NEW.type, NEW.on, NEW.name, NEW.description, NEW.property_data
